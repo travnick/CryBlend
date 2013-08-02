e2736300b1319687314fc26a11b904f6dddc4f24
DuoOratar <duooratar@gmail.com>
2013-08-02 15:25:19 +0100

    Changed version number

--- io_export_cryblend/__init__.py
+++ io_export_cryblend/__init__.py
@@ -32,7 +32,7 @@ bl_info = {
     "name": "CryEngine3 Utilities and Exporter",
     "author": "Angelo J. Miner & Duo Oratar",
     "blender": (2, 6, 7),
-    "version": (4, 9, 3),
+    "version": (4, 9, 4),
     "location": "CryBlend Menu",
     "description": "CryEngine3 Utilities and Exporter",
     "warning": "",
