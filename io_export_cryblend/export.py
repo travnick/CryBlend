7759c5642a0e4aaed9c521993ecef475b1596b1c
Özkan Afacan <feedbackex@gmail.com>
2016-08-02 01:08:05 +0300

    First Layer for BMesh

--- io_export_cryblend/export.py
+++ io_export_cryblend/export.py
@@ -435,9 +435,7 @@ class CrytekDaeExporter:
         parent_element.appendChild(libgeo)
         for group in utils.get_mesh_export_nodes(self._config.export_selected_nodes):
             for object_ in group.objects:
-                utils.set_active(object_)
-                bpy.ops.object.mode_set(mode='EDIT')
-                bmesh_ = bmesh.from_edit_mesh(object_.data)
+                bmesh_, layer_state = utils.get_bmesh(object_)
                 geometry_node = self._doc.createElement("geometry")
                 geometry_name = utils.get_geometry_name(group, object_)
                 geometry_node.setAttribute("id", geometry_name)
@@ -480,7 +478,7 @@ class CrytekDaeExporter:
                 geometry_node.appendChild(mesh_node)
                 libgeo.appendChild(geometry_node)
 
-                bpy.ops.object.mode_set(mode='OBJECT')
+                utils.clear_bmesh(object_, layer_state)
                 cbPrint(
                     '"{}" object has been processed for "{}" node.'.format(
                         object_.name, group.name))
